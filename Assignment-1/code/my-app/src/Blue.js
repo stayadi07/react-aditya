40eead00b828f8b250bbf3c76e7e62b289c6d2cc
stayadi07 <stayadi007@gmail.com>
2022-04-05 02:01:57 +0530

    assignment-1

---
+++ Assignment-1/code/my-app/src/Blue.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blue = () => {
+  return (
+    <div className = "bluecontainer">
+        <div className='blue'></div>
+        <div className='blue'></div>
+        <div className='blue'></div>
+    </div>
+  )
+}
+
+export default Blue;
